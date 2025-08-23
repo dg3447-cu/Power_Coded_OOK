bbd3d52bb17333b1a4831ce344b093faef386e06
dg3447-cu <dg3447@columbia.edu>
2025-08-23 12:56:04 -0400

    Update opt_PWR_code.m

--- opt_PWR_code.m
+++ opt_PWR_code.m
@@ -80,13 +80,6 @@ objective = alpha * log(power) + beta * log(bandwidth);
 
 fprintf('Average voltage: %f (constraint [%f, %f])\n', mean(v), A, B);
 
-figure;
-plot(v, '-o');
-xlabel('Slice');
-ylabel('Voltage Level');
-title('Optimized Signal (Minimizing Power × Bandwidth)');
-grid on;
-
 % Notes:
 % Almost all outputs are symmetric. Only when beta = 0 does the output lose its symmetry.
 
@@ -116,11 +109,39 @@ end
 carrier = sind(2 * pi * Fc .* t);
 s = carrier .* v_extended;
 
-% Compute and plot PSD
+% Compute PSD
 [pxx, f] = pwelch(s, [], [], [], Fs);
+
+% Compute Fourier transform of s
+S = fft(s);
+L = length(s);
+f_fft = Fs * (0:(L-1))/L; % frequency vector for FFT
+S_mag = abs(S)/L; % magnitude normalized
+
+% Plot all figures on one figure using subplots
 figure;
+
+% Plot 1: Optimized voltage configuration
+subplot(3,1,1);
+plot(v, '-o');
+xlabel('Slice');
+ylabel('Voltage Level');
+title('Optimized Signal (Minimizing Power × Bandwidth)');
+grid on;
+
+% Plot 2: PSD of power-coded waveform
+subplot(3,1,2);
 plot(f/1e6, 10 * log10(pxx));
 xlabel('Frequency (MHz)');
 ylabel('Power/Frequency (dB/Hz)');
 title('PSD of Power-Coded OOK Signal');
+grid on;
+
+% Plot 3: Magnitude of Fourier Transform of power-coded waveform
+subplot(3,1,3);
+plot(f_fft/1e6, S_mag);
+xlim([0 max(f_fft)/1e6/2]); % plot only up to Nyquist frequency
+xlabel('Frequency (MHz)');
+ylabel('Magnitude');
+title('Magnitude of Fourier Transform of Signal s');
 grid on;
